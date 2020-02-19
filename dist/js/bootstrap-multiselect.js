47555873afa680606edf0e56a3e89767b6c41225
Ronald van Woensel <rvw@cosninix.com>
2020-02-19 11:18:30 +0100

    incremental

--- dist/js/bootstrap-multiselect.js
+++ dist/js/bootstrap-multiselect.js
@@ -43,15 +43,15 @@
  */
 (function (root, factory) {
     // check to see if 'knockout' AMD module is specified if using requirejs
-    if (typeof define === 'function' && define.amd &&
-        typeof require === 'function' && typeof require.specified === 'function' && require.specified('knockout')) {
-
-        // AMD. Register as an anonymous module.
-        define(['jquery', 'knockout'], factory);
-    } else {
+    // if (typeof define === 'function' && define.amd &&
+    //     typeof require === 'function' && typeof require.specified === 'function' && require.specified('knockout')) {
+    //
+    //     // AMD. Register as an anonymous module.
+    //     define(['jquery', 'knockout'], factory);
+    // } else {
         // Browser globals
         factory(root.jQuery, root.ko);
-    }
+    //}
 })(this, function ($, ko) {
     "use strict";// jshint ;_;
 
